407f8865948cbf52f0bda24bcba5d024176b04b2
Pete Clark <peterclark1996@gmail.com>
2024-06-08 17:02:25 +0100

    Fix post build script

--- packages/create-s2ts/src/postBuild.ts
+++ packages/create-s2ts/src/postBuild.ts
@@ -1,4 +1,4 @@
-import { readFileSync, unlinkSync, writeFileSync } from "fs"
+import { existsSync, readFileSync, unlinkSync, writeFileSync } from "fs"
 import path from "path"
 
 const indexFilePath = path.join(process.cwd(), "dist/index.js")
@@ -6,4 +6,7 @@ const fileContent = readFileSync(indexFilePath, "utf8")
 const fileContentWithVersion = fileContent.replace("process.env.npm_package_version", `"${process.env.npm_package_version}"`)
 writeFileSync(indexFilePath, fileContentWithVersion)
 
-unlinkSync(path.join(process.cwd(), "dist/postBuild.js"))
+const fileToRemovePath = path.join(process.cwd(), "dist/postBuild.js")
+if (existsSync(fileToRemovePath)) {
+    unlinkSync(fileToRemovePath)
+}
