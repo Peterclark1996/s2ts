94f11ce13d22cc2d618dfa692295f8aab6eaf1d6
Pete Clark <peterclark1996@gmail.com>
2024-07-31 23:38:57 +0100

    Allow running at specified path

--- packages/s2ts/src/cli.ts
+++ packages/s2ts/src/cli.ts
@@ -3,10 +3,12 @@
 import { start } from "./index"
 
 const command = process.argv[2]
+const argument = process.argv[3]
 
 switch (command) {
+    case "dev":
     case "start":
-        start()
+        start(argument)
         break
     default:
         console.log(`Unknown command: ${command}. Did you mean 'start'?`)
--- packages/s2ts/src/index.ts
+++ packages/s2ts/src/index.ts
@@ -3,15 +3,13 @@ import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs"
 import path from "path"
 import { compileVtsFile } from "./compile"
 
-const watchDir = path.join(process.cwd(), "./scripts").replace(/[\\/]+/g, "/")
-
 const sourcePathPart = "/content/csgo_addons"
 const targetPathPart = "/game/csgo_addons"
 
 const processFile = (filePath: string) => {
     if (!filePath.endsWith(".vts")) return
 
-    const standardFilePath = filePath.replace(/[\\/]+/g, "/")
+    const standardFilePath = standardisePath(filePath)
 
     const data = readFileSync(standardFilePath).toString("utf-8")
     const compiledBuffer = compileVtsFile(data)
@@ -25,7 +23,9 @@ const processFile = (filePath: string) => {
     console.log(`${now.toLocaleTimeString()} Compiled: ${path.basename(outputFilePath)}`)
 }
 
-export const start = () => {
+export const start = (specifiedPath: string | undefined) => {
+    const watchDir = standardisePath(getWatchDir(specifiedPath))
+
     if (!watchDir.includes(sourcePathPart)) {
         console.error(
             `The current path '${watchDir}' was expected to contain '${sourcePathPart}' but it did not. Check if you are running in the correct folder.`
@@ -45,3 +45,17 @@ export const start = () => {
 
     console.log(`Watching for file changes in ${watchDir}`)
 }
+
+const getWatchDir = (specifiedPath: string | undefined) => {
+    if (specifiedPath === undefined) {
+        return path.join(process.cwd(), "./scripts")
+    }
+
+    if (specifiedPath.endsWith("/scripts") || specifiedPath.endsWith("\\scripts")) {
+        return specifiedPath
+    }
+
+    return path.join(specifiedPath, "./scripts")
+}
+
+const standardisePath = (fullPath: string) => fullPath.replace(/[\\/]+/g, "/")
