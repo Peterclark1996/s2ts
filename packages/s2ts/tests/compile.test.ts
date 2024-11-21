9cc1f5988b5d6b859ce3d81cc5c32f41f0ba8d29
Pete <peterclark1996@gmail.com>
2024-11-21 15:48:38 +0800

    Add failing test step to CI

--- packages/s2ts/tests/compile.test.ts
+++ packages/s2ts/tests/compile.test.ts
@@ -2,6 +2,10 @@ import { readFileSync } from "fs"
 import path from "path"
 import { processFileData } from "../src/cli"
 
+test("Failing test to prove CI works", () => {
+    throw new Error("This is a failing test")
+})
+
 test("I can compile a vts file and get a correctly formed vts_c file", async () => {
     const sourcePath = path.join(__dirname, "/resource/test.vts")
     const content = readFileSync(sourcePath).toString("utf-8")
